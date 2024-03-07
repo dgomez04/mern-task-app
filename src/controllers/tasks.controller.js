2fef138261b2080663440e9fa3a793772c0469fb
dgomez04 <dagomezmoreno@outlook.com>
2024-03-06 19:10:56 -0600

    added subtasks to controller logic

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -7,12 +7,13 @@ export const getTasks = async (req, res) => {
 };
 
 export const createTask = async (req, res) => {
-    const { title, description, date } = req.body;
+    const { title, description, date, subtasks } = req.body;
 
     const newTask = new Task({
         title,
         description,
         date,
+        subtasks,
         user: req.user.id
     });
 
@@ -39,3 +40,44 @@ export const getTask = async (req, res) => {
 
     res.json(task);
 };
+
+export const createSubstask = async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    const { title, description} = req.body;
+    tasks.subtasks.push({ title, description });
+
+    const saved_task = await task.save();
+    res.json(saved_task);
+}
+
+export const getSubtasks = async (req ,res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    res.json(task.subtasks);
+}
+
+export const deleteSubtasks = async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    task.subtask.id(req.params.subtaskId).remove();
+    const saved_task = await task.save();
+
+    res.json(saved_task);
+}
+
+export const updateSubtask = async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    const subtask = task.subtasks.id(req.params.subtaskId);
+    if (!subtask) return res.status(404).json({ message: "Subtask not found" });
+
+    subtask.set(req.body);
+    const saved_task = await task.save();
+
+    res.json(saved_task);
+}
