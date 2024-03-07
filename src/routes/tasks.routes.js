b9d3aac9a2b428ca3e0350e9cfd66e0fed2fb2b0
dgomez04 <dagomezmoreno@outlook.com>
2024-03-06 19:18:25 -0600

    added subtask routes

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authRequired } from '../middlewares/validateToken.js';
-import { getTasks, getTask, updateTask, deleteTask, createTask } from "../controllers/tasks.controller.js";
+import { getTasks, getTask, updateTask, deleteTask, createTask, createSubstask, getSubtasks, deleteSubtasks, updateSubtask } from "../controllers/tasks.controller.js";
 
 const router = Router();
 
@@ -9,6 +9,12 @@ router.get('/tasks/:id', authRequired,getTask);
 router.post('/tasks', authRequired,createTask);
 router.delete('/tasks/:id', authRequired,deleteTask);
 router.put('/tasks/:id', authRequired,updateTask);
+
+router.post('/tasks/:id/subtasks', authRequired,createSubstask);
+router.get('/tasks/:id/subtasks', authRequired,getSubtasks);
+router.delete('/tasks/:id/subtasks/:subtaskId', authRequired,deleteSubtasks);
+router.put('/tasks/:id/subtasks/:subtaskId', authRequired,updateSubtask);
+
 
 
 export default router;
